6ace33716731b60fdbad1911c7a44709a26376c9
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-06-01 13:14:32 +0200

    enable logging out and minor code reformatting

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import * as algoliasearch from 'algoliasearch'
-import * as env from '../../env'
+import * as env from '../../env';
 import { Item } from '../../models/item';
 
 @IonicPage()
@@ -10,48 +10,44 @@ import { Item } from '../../models/item';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
-  client: any;
-  index: any;
-  ALGOLIA_APP_ID: string = env.algolia.ALGOLIA_APP_ID
-  ALGOLIA_API_KEY: string = env.algolia.ALGOLIA_SEARCH_KEY
-  searchQuery: string = "";
-  items: any[] = [];
+  private client: algoliasearch.Client;
+  private index: algoliasearch.Index;
+  public searchQuery: string = "";
+  public items: any[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.client = algoliasearch(this.ALGOLIA_APP_ID, this.ALGOLIA_API_KEY, { protocol: 'https:' });
-    this.index = this.client.initIndex("Marketplace")
+    this.client = algoliasearch(env.algolia.ALGOLIA_APP_ID, env.algolia.ALGOLIA_SEARCH_KEY, { protocol: 'https:' });
+    this.index = this.client.initIndex("Marketplace");
   }
 
-  search(event) {
-    this.index.search({
-      query: this.searchQuery
-    }).then((data) => {
-      console.log(data.hits)
-      this.items = data.hits;
-
-    })
+  search() {
+    this.index
+      .search({ query: this.searchQuery })
+      .then((data) => this.items = data.hits);
   }
 
   getCategory(cat: string) {
-    console.log(cat);
     this.navCtrl.push('CategoryOverviewPage', { 'type': cat });
   }
 
   goToStore(child?) {
-
     if (child) {
-      this.navCtrl.setRoot('StorePage', { child: child });
+      this.navCtrl.setRoot('StorePage', { 'child': child });
     } else {
       this.navCtrl.setRoot('StorePage');
     }
-
   }
+
   pushWishlistPage() {
     this.navCtrl.push('WishlistPage');
   }
+
   pushToDetailPage(item: Item) {
-    console.log(item);
     this.navCtrl.push('ItemDetailPage', { 'item': item });
   }
+
+  logout(): void {
+    localStorage.removeItem('user');
+  }
+
 }
